c069e2f6c771d76bf86cb576384eaf8477cfb818
Abdullah Iqbal <ai3449713@gmail.com>
2021-10-26 11:29:33 +0500

    signup, index(login), index(search)

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,25 +1,43 @@
-function myFunction() {
-    document.getElementById("myDropdown").style.display = "block";
-    console.log("run");
-}
-
 window.onclick = function (event) {
 
-    console.log(event.target);
-    
+  console.log(event.target);
 
-    if (!event.target.matches('.abc')) {
-        var dropdowns = document.getElementById("myDropdown");
-        dropdowns.style.display='none';
-        console.log("end"); 
-       
-    }
+
+  if (!event.target.matches('.abc')) {
+    var dropdowns = document.getElementById("myDropdown");
+    dropdowns.style.display = 'none';
+    console.log("end");
+
+  }
+
+  if (!event.target.matches('.search') && !event.target.matches('.unsplash-input')) {
+    document.getElementById("search").style.display = "none";
+    console.log("end search");
+  }
+
+
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 
 
+
+
+
+
+function myFunction() {
+  document.getElementById("myDropdown").style.display = "block";
+  console.log("run");
+}
+
+
+
+
+// login page 
 function loginPage() {
-    window.location.href = "E:/JMM/4th Day Task/Unsplash Task/signUp.html";
+  window.location.href = "E:/JMM/4th Day Task/Unsplash Task/signUp.html";
 }
 
 // ----------------------------model js------------------------- 
@@ -27,18 +45,19 @@ function loginPage() {
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
   console.log("run");
 }
 
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
   console.log("end");
 }
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+
+// search dropdown 
+function search_Dropdeown() {
+  document.getElementById("search").style.display = "block";
+  console.log("run search");
 }
